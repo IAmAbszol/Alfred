94da9f1f90b4077756c61ff7cb08399baec6e357
Kyle Darling <kdarling95@yahoo.com>
2020-12-16 07:27:48 -0500

    Cleaned Up Receiver
    The network receiver has finally been cleaned up and video reintroduced within the latest Process -> Thread switch over. __getstate__ will remain until deemed completely unnecessary as currently it provides a depiction on how to handle Process/pickling.

--- meleeai/framework/network/receiver.py
+++ meleeai/framework/network/receiver.py
@@ -5,23 +5,15 @@ import multiprocessing
 import socket
 import time
 
-from concurrent import futures
 from io import BytesIO
+from threading import Thread
 
-from meleeai.utils.message_type import MessageType
+from meleeai.memory import get_memory
 from meleeai.utils.controller_parser import ControllerParser
+from meleeai.utils.data_class import ControllerData, SlippiData, VideoData
+from meleeai.utils.message_type import MessageType
 from meleeai.utils.slippi_parser import SlippiParser
 from meleeai.utils.video_parser import VideoParser
-
-from meleeai.framework.network.sender import NetworkSender
-import json
-import numpy as np
-from copy import deepcopy
-from meleeai.utils.util import getsize
-from meleeai.memory import get_memory
-from meleeai.memory.circular_buffer import CircularBuffer
-from meleeai.utils.data_class import ControllerData, SlippiData
-from threading import Thread
 
 
 class NetworkReceiver():
@@ -34,6 +26,8 @@ class NetworkReceiver():
             get_memory().create(obj=ControllerData(MessageType.CONTROLLER, datetime.datetime.utcnow(), {}))
         self._slippi_memory_name, self._slippi_circular_buffer = \
             get_memory().create(obj=SlippiData(MessageType.CONTROLLER, datetime.datetime.utcnow(), {}))
+        self._video_memory_name, self._video_circular_buffer = \
+            get_memory().create(obj=VideoData(MessageType.VIDEO, datetime.datetime.utcnow(), {}))
 
         # Global fields
         self._flags             = absl.flags.FLAGS
@@ -41,7 +35,8 @@ class NetworkReceiver():
         # key -> [function, circular_buffer, process (Defaulted to None)]
         self._active_functions  = {
             'controller': [self._listen_controller, self._controller_circular_buffer, None],
-            'slippi': [self._listen_slippi, self._slippi_circular_buffer, None]
+            'slippi': [self._listen_slippi, self._slippi_circular_buffer, None],
+            'video': [self._listen_video, self._video_circular_buffer, None]
         }
 
         self._namespace         = multiprocessing.Manager().Namespace()
@@ -64,15 +59,12 @@ class NetworkReceiver():
 
     def _listen_controller(self):
         """Listens to the controller socket for any data sent from the Dolphin Emulator
-        configured for said adresss & port in flags.
+        configured for said addresss & port in flags.
         """
         controller_parser = ControllerParser()
         controller_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         controller_socket.bind(('', self._flags.controllerport))
         controller_socket.settimeout(1)
-        ns = NetworkSender()
-        controller_memory = CircularBuffer(obj=ControllerData(MessageType.CONTROLLER, datetime.datetime.utcnow(), {}), \
-                                           shared_memory_name=self._controller_memory_name)
         while self._namespace.run:
             try:
                 data_str, _         = controller_socket.recvfrom(2048)
@@ -80,8 +72,6 @@ class NetworkReceiver():
                 if controller_data:
                     timestamp = float(f'{controller_data["timestamp_sec"]}.{controller_data["timestamp_micro"]}')
                     self._controller_circular_buffer.write(ControllerData(MessageType.CONTROLLER, datetime.datetime.utcfromtimestamp(timestamp), controller_data))
-                    if controller_data['device_number'] == 1:
-                        ns.send(bytes(json.dumps(controller_data), encoding='utf-8'))
             except socket.timeout:
                 logging.warning('Failed to receive any data from controller socket.')
             except Exception as excp:
@@ -91,22 +81,19 @@ class NetworkReceiver():
 
     def _listen_slippi(self):
         """Listens to the slippi socket for any data sent from the Dolphin Emulator
-        configured for said adresss & port in flags.
+        configured for said addresss & port in flags.
         """
         slippi_parser     = SlippiParser()
         slippi_socket     = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         slippi_socket.bind(('', self._flags.slippiport))
         slippi_socket.settimeout(1)
-        #slippi_memory = CircularBuffer(obj=SlippiData(MessageType.SLIPPI, datetime.datetime.utcnow(), {}), \
-        #                               shared_memory_name=self._slippi_memory_name)
         while self._namespace.run:
             try:
                 data_str, _         = slippi_socket.recvfrom(1024)
                 events              = slippi_parser.parse_bin(BytesIO(data_str))
                 if events:
                     for timestamp, event in events:
-                        self._slippi_memory.write_memory(SlippiData(MessageType.SLIPPI, datetime.datetime.utcfromtimestamp(timestamp), event))
-                    logging.info(events)
+                        self._slippi_circular_buffer.write(SlippiData(MessageType.SLIPPI, datetime.datetime.utcfromtimestamp(timestamp), event))
             except socket.timeout:
                 logging.warning('Failed to receive any data from slippi socket.')
             except OSError as os_error:
@@ -115,26 +102,25 @@ class NetworkReceiver():
                 logging.warning(f'Slippi crashed. Error: {excp}.')
         slippi_socket.close()
 
-    def _listen_video(self, namespace, receiver_list):
-        pass
-        """
+    def _listen_video(self):
         video_parser      = VideoParser()
         video_socket      = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        video_socket.bind(('', self.videoport))
+        video_socket.bind(('', self._flags.videoport))
         video_socket.settimeout(1)
-        while parent._namespace.run:
+        while self._namespace.run:
             try:
                 data_str, _ = video_socket.recvfrom((2**16) - 1)
                 if video_parser.update(data_str):
                     video_captures = video_parser.get_completed_images()
-                    if video_captures and len(parent._receiver_list) <= self.receiver_buffer:
-                        parent._receiver_list.append((MessageType.VIDEO, video_captures[0]))
+                    if video_captures:
+                        # TODO: Try in-line lambda expression to convert x to datetime and leave y.
+                        timestamp, data = video_captures[0]
+                        self._video_circular_buffer.write(VideoData(MessageType.VIDEO, datetime.datetime.utcfromtimestamp(timestamp), data))
             except socket.timeout:
                 logging.warning('Failed to receive any data from video socket.')
             except OSError:
                 logging.warning('Video receiver pipeline has been closed.')
         video_socket.close()
-        """
 
     def collect(self):
         for name in self._active_functions:
